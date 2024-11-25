f5ba34b797376cfd47970171a35d2ec2fcc16c89
Oliulla <mohammad.oliullah25@gmail.com>
2024-11-26 00:05:55 +0600

    Added generate metadata

--- src/app/(pages)/layout.js
+++ src/app/(pages)/layout.js
@@ -4,11 +4,61 @@ import 'sweetalert2/src/sweetalert2.scss';
 import Footer from "@/components/layout/Footer/Footer";
 
 import "../globals.css";
-
+import { projectfor } from "@/constants/projectfor";
+import { headers } from "next/headers";
 
 const roboto = Roboto_Slab({ subsets: ['latin'], display: 'swap', adjustFontFallback: false });
 
+export async function generateMetadata() {
 
+  const headerList = headers();
+  const pathname = headerList.get("x-current-path");
+  const clientUrlWithPath = process.env.NEXT_PUBLIC_CLIENT_URL + pathname
+
+
+  try {
+    const apiUrl = process.env.NEXT_PUBLIC_API_URL;
+
+    const response = await fetch(`${apiUrl}/api/metadata?projectFor=${projectfor}&pageLink=${clientUrlWithPath}`, {
+      cache: "no-store",
+    });
+    const data = await response.json();
+
+    const { title, description, keywords } = data?.data[0] ?? {};
+
+    const gglverificationResponse = await fetch(`${apiUrl}/api/site-verification?projectFor=${projectfor}`, {
+      cache: "no-store",
+    });
+
+    const gVerificationData = await gglverificationResponse.json();
+
+    const verificationContent = gVerificationData?.data?.[0]?.url
+
+    return {
+      title: title || "Rain Coupon",
+      description: description || "Rain Coupon",
+      keywords: keywords || "Rain Coupon",
+      openGraph: {
+        title: title || "Rain Coupon",
+        description: description || "Rain Coupon",
+      },
+      verification: {
+        google: verificationContent || "",
+      },
+      alternates: {
+        canonical: clientUrlWithPath,
+      },
+      robots: "index, follow, max-image-preview:large, max-snippet:-1, max-video-preview:-1",
+    };
+
+  } catch (error) {
+    return {
+      title: "Home",
+      description: "Home",
+      keywords: "Rain Coupon",
+    }
+  }
+}
 
 export default function RootLayout({ children }) {
   return (
